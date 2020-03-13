187b6fc08767d53129ca721c36ba7dbca17e2c66
Hanna Lee <mani21na@gmail.com>
2020-03-13 16:22:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here
 function findMatching(drivers, string) {
-  return drivers.filter( d => d.toLoweCase() === string.toLoweCase() )
+  return drivers.filter( drivers => drivers.toLoweCase() === string.toLoweCase() )
 }
 
 findMatching = (drivers, string) => {
