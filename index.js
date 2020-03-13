2239dd6ab388e1a8dab8490b3c92567ccecb528e
Hanna Lee <mani21na@gmail.com>
2020-03-13 16:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Code your solution here
+
 //function findMatching(drivers, string) {
 //  return drivers.filter( driver => driver.toLoweCase() === string.toLoweCase() )
 //}
 
 findMatching = (drivers, string) => {
   return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(
+    driver => driver.slice(0).toLowerCase() === string.toLowerCase()
+  )
 }
