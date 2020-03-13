415efc3dc5c13a7aa74c071960cd9a438918a2b3
Hanna Lee <mani21na@gmail.com>
2020-03-13 16:22:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ function findMatching(drivers, string) {
   return drivers.filter( drivers => drivers.toLoweCase() === string.toLoweCase() )
 }
 
-findMatching = (drivers, string) => {
-  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
-}
